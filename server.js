2dd0c4b242981d6171bc74d3e4e1df950b18362b
SoftwareGrokkers <99093761+SoftwareGrokkers@users.noreply.github.com>
2022-02-21 19:35:47 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.get('/activties', function(req,res){
     var activties = get_activities();
     
 })
-var get_activities = function(){
-    //TODO
-    return;
-}
+
 
 app.post('/process-login', function(req,res){
     //TODO
@@ -39,3 +36,114 @@ app.post('upload-activty', function(req,res){
 app.listen(app.get("port"), function(){
 	console.log("server running")
 })
+
+class login-signup{
+	
+	var process-signup = function(){
+		// process signup
+	}
+	
+	var process-login = function(){
+		// process login
+	}
+	
+	var get-preference = function(){
+		// get user preference
+	}
+	
+	var go-to-homepage = function(){
+		// go to homepage
+	}
+}
+
+
+
+class connect-to-database{
+	var create_db_connection = function(){
+		// connect to database
+	}
+	var close_db_connection = function(){
+		// close database connection
+	}
+}
+
+class Homepage{
+	var show-groups = function(){
+		// show user groups
+	}
+	
+	var sort-activites = function(){
+		// sort activites
+	}
+	
+	var get_activities-data = function(){
+		// get activites data
+	}
+	
+	
+}
+class chat-room{
+	var create-chat-room = function(){
+		// create chat room
+	}
+	
+	var close-chat-room = function(){
+		
+	}
+}
+class Group-chat{
+	var create-chat-room = function(){
+		// create chat room
+	}
+	
+	var close-chat-room = function(){
+		// close chat room
+	}
+	
+	var get_activities = function(){
+		//TODO
+	}
+	
+}
+class Private-chat{
+	var create-chat-room = function(){
+		// create chat room
+	}
+	
+	var close-chat-room = function(){
+		
+	}
+}
+
+class Group{
+	var show-groups = function(){
+		// show user groups
+	}
+	
+}
+
+class Sort-Groups{
+	var sort-activites = function(){
+		// sort activites
+	}
+	
+	var get_activities = function(){
+		//TODO
+	}
+	
+	var sort-location = function(){
+		// sort location
+	}
+	var sort-time = function(){
+		// sort time
+	}
+}
+
+class search{
+	var search-activites = function(){
+		// search activities
+	}
+	var search-groups = function(){
+		// search groups
+	}
+}
