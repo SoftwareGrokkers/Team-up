bea2ee3ce62fe5f51cec7fcf9a92539113f9a80c
Tomi-Ola <99094563+Tomi-Ola@users.noreply.github.com>
2022-03-12 16:54:19 -0600

    Added db-connection script

---
+++ db-connection.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose");
+
+var userBuilder = mongoose.Schema({
+	
+    firstname: String,
+    lastname: String,
+    email: String,
+    phone: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: Number,
+    activity: String,
+    
+});
+
+var users = mongoose.model("Users", userBuilder);
+module.exports = users;
