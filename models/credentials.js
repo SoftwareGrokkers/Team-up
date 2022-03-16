20eca4f75da638d18aa936996d9251096da043b2
Tomi-Ola <tomilols.to@gmail.com>
2022-03-15 20:20:10 -0400

    worked on login and signup

---
+++ models/credentials.js
@@ -0,0 +1,12 @@
+module.exports = {
+	// cookieSecret: "this is the cookie secret",
+	// gmail: {
+		// user: "tomilols.to2@gmail.com",
+		// password: "password",
+	// },
+	mongo: {
+		db: {
+			connectionString: "mongodb+srv://softwaregrokkers:grokkerssoftware@cluster0.xa1km.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+		}
+	},
+};
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = express()
-var fs = require("fs")
+// var fs = require("fs")
+var credentials = require("./models/credentials.js")
+// var handlebars = require("express-handlebars").create({defaultLayout: "main"})
+var Team_upUser = require("./Team-upSchema.js")
+// app.engine("handlebars", handlebars.engine)
+// app.set("view engine", "handlebars")
+app.use(require("body-parser")());
 
 
 app.set('port', process.env.port || 3000)
@@ -19,12 +25,56 @@ app.get('/activties', function(req,res){
 
 
 app.post('/process-login', function(req,res){
-    //TODO
+    userConnectionString = credentials.mongo.db.connectionString
+    mongoose.connect(userConnectionString)
+    
+    Team_upUser.find({email: req.body.email}, function(err, Team_upUser){
+        if (err){
+            //TODO handle error
+            console.log(err);
+            return;
+        }
+        
+        if(Team_upUser[0].password === req.body.password){
+            console.log("login successful")
+        }
+        else{
+            console.log("incorrect password")
+        }
+        
+        
+    })
     
 })
 
 app.post('/process-signup', function(req,res){
-    //TODO
+    //TODO use class instead
+    userConnectionString = credentials.mongo.db.connectionString
+    mongoose.connect(userConnectionString)
+    
+    Team_upUser.find({email: req.body.email}, function(err, Team_upUser){
+        if (err){
+            //TODO handle error
+            console.log(err);                     
+            return;
+        }
+        
+        if (Team_upUser.length){
+            //TODO write error to screen
+            console.log("user aleady exists");
+            //return maybe?
+            return;
+        }
+        
+        new Team_upUser({
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            email: req.body.email,
+            password: req.body.password //TODO encript password
+        })
+    })
+    
+    
     
 })
 
@@ -37,142 +87,142 @@ app.listen(app.get("port"), function(){
 	console.log("server running")
 })
 
-class login-signup{
+// class login-signup{
 	
-	var authenticate-signup = function(){
-		// process signup
-	}
+	// var authenticate-signup = function(){
+		// // process signup
+	// }
 	
-	var authenticate-login = function(){
-		// process login
-	}
+	// var authenticate-login = function(){
+		// // process login
+	// }
 	
-	var get-preference = function(){
-		// get user preference
-	}
+	// var get-preference = function(){
+		// // get user preference
+	// }
 	
-	var go-to-homepage = function(){
-		// go to homepage
-	}
-}
+	// var go-to-homepage = function(){
+		// // go to homepage
+	// }
+// }
 
 
 
-class Database_Manager{
-	var create_db_connection = function(){
-		// connect to database
-	}
-	var close_db_connection = function(){
-		// close database connection
-	}
+// class Database_Manager{
+	// var create_db_connection = function(){
+		// // connect to database
+	// }
+	// var close_db_connection = function(){
+		// // close database connection
+	// }
 	
-	var retrive_data = function(){
-		// retrives data
-	}
+	// var retrive_data = function(){
+		// // retrives data
+	// }
 	
-	var send_data = function(){
-		// sends data
-	}
-}
+	// var send_data = function(){
+		// // sends data
+	// }
+// }
 
-class Homepage{
-	var show-groups = function(){
-		// show user groups
-	}
+// class Homepage{
+	// var show-groups = function(){
+		// // show user groups
+	// }
 	
-	var get-user-preferences = function(){
-		// get user preferences
-	}
+	// var get-user-preferences = function(){
+		// // get user preferences
+	// }
 	
-	var log-out = function(){
-		// get activites data
-	}
+	// var log-out = function(){
+		// // get activites data
+	// }
 	
-	var login-signup = function(){
-		// show login/signup page
-	}
+	// var login-signup = function(){
+		// // show login/signup page
+	// }
 	
 	
-}
+// }
 
-}
-class Groups{
-	var create-chat-room = function(){
-		// create chat room
-	}
+// }
+// class Groups{
+	// var create-chat-room = function(){
+		// // create chat room
+	// }
 	
-	var close-chat-room = function(){
-		// close chat room
-	}
+	// var close-chat-room = function(){
+		// // close chat room
+	// }
 	
-	var go-to-homepage = function(){
-		//TODO
-	}
+	// var go-to-homepage = function(){
+		// //TODO
+	// }
 	
-}
-
-
-class Chat-Room{
-	var create-chat-room = function(){
-		// create chat room
-	}
-	
-	var close-chat-room = function(){
-		// close chat room
-	}
-	
-	var send-chat-data = function(){
-		// send chat data
-	}
-	
-	var retrive-chat-data = function(){
-		// retrieve chat data
-	}
-}
-
-class Chat-Manager{
-	var create_db_connection = function(){
-		// connect to database
-	}
-	var close_db_connection = function(){
-		// close database connection
-	}
-	
-	var retrive_data = function(){
-		// retrives data
-	}
-	
-	var send_data = function(){
-		// sends data
-	}
-}
+// }
 
 
-class User-preferences{
-	class Sort-Activities{
-		var sort-activites = function(){
-			// sort activites
-		}
+// class Chat-Room{
+	// var create-chat-room = function(){
+		// // create chat room
+	// }
+	
+	// var close-chat-room = function(){
+		// // close chat room
+	// }
+	
+	// var send-chat-data = function(){
+		// // send chat data
+	// }
+	
+	// var retrive-chat-data = function(){
+		// // retrieve chat data
+	// }
+// }
+
+// class Chat-Manager{
+	// var create_db_connection = function(){
+		// // connect to database
+	// }
+	// var close_db_connection = function(){
+		// // close database connection
+	// }
+	
+	// var retrive_data = function(){
+		// // retrives data
+	// }
+	
+	// var send_data = function(){
+		// // sends data
+	// }
+// }
+
+
+// class User-preferences{
+	// class Sort-Activities{
+		// var sort-activites = function(){
+			// // sort activites
+		// }
 		
-		var get_activities = function(){
-			//TODO
-		}
+		// var get_activities = function(){
+			// //TODO
+		// }
 		
-		var sort-location = function(){
-			// sort location
-		}
-		var sort-time = function(){
-			// sort time
-		}
-	}
+		// var sort-location = function(){
+			// // sort location
+		// }
+		// var sort-time = function(){
+			// // sort time
+		// }
+	// }
 	
-	class Search-Activities{
-		var search-activites = function(){
-			// search activities
-		}
-		var search-groups = function(){
-			// search groups
-		}
-	}
-}
+	// class Search-Activities{
+		// var search-activites = function(){
+			// // search activities
+		// }
+		// var search-groups = function(){
+			// // search groups
+		// }
+	// }
+// }
 
