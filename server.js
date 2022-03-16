47d6fc895ddbed4262ad540a4b896cc0b65754ce
Tomi-Ola <tomilols.to@gmail.com>
2022-03-16 16:25:47 -0400

    mad minor changes

--- server.js
+++ server.js
@@ -98,6 +98,20 @@ app.post('upload-activty', function(req,res){
     
 })
 
+
+app.use(function(req, res){
+	
+	res.status(404)
+	res.render('404')
+})
+
+
+app.use(function(err, req, res, next){
+	console.error(err.stack)
+	res.status(500)
+	res.render("500")
+})
+
 app.listen(app.get("port"), function(){
 	console.log("server running")
 })
