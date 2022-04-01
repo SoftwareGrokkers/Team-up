822ee302dafe0c3ca16b5741763efaa5858e56d4
Geoff24 <81037152+Geoff24@users.noreply.github.com>
2022-04-01 17:00:36 -0500

    Add files via upload
    Divides activities and groups

--- frontEnd/groupsScript.js
+++ frontEnd/groupsScript.js
@@ -1,7 +1,8 @@
+// Load user groups
 var groupsHome = new XMLHttpRequest();
 groupsHome.open('GET', '#');
 
-ourRequest.onload = function () {
+groupsHome.onload = function () {
   var groupsData = JSON.parse(groupsHome.responseText);
   if (groupsData.length == 0) {
     console.log("Add a group");
@@ -12,8 +13,30 @@ ourRequest.onload = function () {
       var groupdiv = document.createElement('div');
       groupdiv.classList.add('group');
       allGroupsDiv.appendChild(groupdiv);
+
+    }
+  }
+};
+groupsHome.send();
+
+
+// Load user activities
+var activitiesHome = new XMLHttpRequest();
+activitiesHome.open('GET', '#');
+
+activitiesHome.onload = function () {
+  var activitiesData = JSON.parse(activitiessHome.responseText);
+  if (activitiesData.length == 0) {
+    console.log("Add an activity");
+  }
+  else {
+    for (let i = 0; i < groupsData.length; i++) {
+      var allActivitiesDiv = document.getElementById('activities');
+      var activityDiv = document.createElement('div');
+      activityDiv.classList.add('activity');
+      allActivitiesDiv.appendChild(activityDiv);
     }
   }
 };
 
-groupsHome.send();
+activitiesHome.send();
