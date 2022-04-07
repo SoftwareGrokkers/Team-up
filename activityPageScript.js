9ba2264874385416688d594433968f4a37c3e951
Geoff24 <81037152+Geoff24@users.noreply.github.com>
2022-04-06 19:15:46 -0500

    Add files via upload

---
+++ activityPageScript.js
@@ -0,0 +1,64 @@
+// Open and close nav bar
+const navBtn = document.getElementById("navBtn");
+const navFull = document.getElementById("navFull");
+const navClose = document.getElementById("navClose");
+
+navBtn.addEventListener("click", () => {
+  navFull.classList.add("showNav");
+});
+
+navClose.addEventListener("click", () => {
+  navFull.classList.remove("showNav");
+});
+
+var getActivityCookie = function(){
+    Cookies = document.cookie.split(";")
+    groupCookie = Cookies[1]
+    goodCookie = groupCookie.replace("%20", " ")
+    return goodCookie
+}
+
+var activityCookie = getActivityCookie()
+
+var request = new XMLHttpRequest();
+const userActivitiesUrl = `http://localhost:3000/userActivityPage?cookie=${activityCookie}`
+request.open('GET', userActivitiesUrl,false)
+request.send()
+
+try{
+    var activityData = JSON.parse(request.responseText);
+    console.log(activityData)
+}
+catch(error){
+    var activityData = {Name: "Football Game", type : "Sport", description: "its a sport", creator: "John Smith", location: "Baton Rouge", time: "12 PM"}
+}
+
+var activityInfo = document.getElementById('activityInfo')
+var activityName = document.createElement("h1");
+var activitytype = document.createElement("h3")
+var activityDescription = document.createElement("p")
+var activityCreator = document.createElement("p")
+var activityLocation = document.createElement("h2")
+var activityTime = document.createElement("h2")
+
+activityName.appendChild(document.createTextNode(activityData.Name));
+activitytype.appendChild(document.createTextNode(activityData.type));
+activityLocation.appendChild(document.createTextNode(activityData.location))
+activityTime.appendChild(document.createTextNode(activityData.time))
+activityDescription.appendChild(document.createTextNode(activityData.description));
+activityCreator.appendChild(document.createTextNode("Creator: " + activityData.creator));
+
+activityInfo.appendChild(activityName)
+activityInfo.appendChild(activityLocation)
+activityInfo.appendChild(activityTime)
+activityInfo.appendChild(activitytype)
+activityInfo.appendChild(activityDescription)
+activityInfo.appendChild(activityCreator)
+
+
+var join = document.getElementsByClassName("button")[0];
+join.addEventListener("click", joinGroup)
+
+function joinGroup() {
+  // TODO
+}
